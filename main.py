4fd2e1bc6781325f5609349820624b410a0a5f72
zhangwei <zhangwei24@apache.org>
2022-07-03 12:00:06 +0800

    Add some redis usage examples

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
+import json
+import os
 import random
 import time
 
+import redis
 import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
+r = redis.Redis(host=os.getenv("REDIS_HOST", "127.0.0.1"), port=6379)
 
 
-@app.get('/artist')
-@app.post('/artist')
+@app.get('/app')
+@app.post('/app')
 async def application():
     time.sleep(random.random())
-    return {'artist': 'Luis Fonsi'}
+    obstacle = r.get("obstacle")
+    if obstacle is None or obstacle.isspace():
+        return "is space"
+    return json.loads(obstacle)
+
+
+@app.get("/read")
+async def read():
+    with open('obstacle.json', 'r') as obstacle:
+        event = json.load(obstacle)
+        r.set("obstacle", json.dumps(event))
+    return "read"
+
+
+@app.get("/delete")
+async def rm():
+    r.delete("obstacle")
+    return "deleted"
 
 
 if __name__ == '__main__':
